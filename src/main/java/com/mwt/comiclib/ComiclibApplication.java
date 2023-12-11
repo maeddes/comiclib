6dfacb6ee744db8fe102aa9ce44a59583f809462
ElenaLoehnert <31loel1dpt@hft-stuttgart.de>
2023-12-11 11:06:36 +0100

    First Commit

---
+++ src/main/java/com/mwt/comiclib/ComiclibApplication.java
@@ -0,0 +1,13 @@
+package com.mwt.comiclib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComiclibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComiclibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwt/comiclib/ComiclibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwt.comiclib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComiclibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
