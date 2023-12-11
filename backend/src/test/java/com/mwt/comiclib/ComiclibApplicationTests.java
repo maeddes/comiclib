eeffaa3fe7d2510b1fa853250285732764cd3770
ElenaLoehnert <31loel1dpt@hft-stuttgart.de>
2023-12-11 17:29:11 +0100

    explicitly defined network... commented currently not implemented test

--- backend/src/test/java/com/mwt/comiclib/ComiclibApplicationTests.java
+++ backend/src/test/java/com/mwt/comiclib/ComiclibApplicationTests.java
@@ -3,6 +3,7 @@ package com.mwt.comiclib;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
 @SpringBootTest
 class ComiclibApplicationTests {
 
@@ -11,3 +12,4 @@ class ComiclibApplicationTests {
 	}
 
 }
+*/
