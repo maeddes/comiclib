e12ab658a24cf6fceaa80b5089b652881c4c1205
maeddes <maeddes@gmail.com>
2023-12-22 10:59:03 +0000

    docker multistage & healthcheck

--- backend/src/main/java/com/mwt/controller/ComicController.java
+++ backend/src/main/java/com/mwt/controller/ComicController.java
@@ -1,14 +1,24 @@
 package com.mwt.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.mwt.model.ComicEntity;
 import com.mwt.repository.ComicRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/comics")
+@CrossOrigin(origins = "*")
 public class ComicController {
 
     private final ComicRepository comicRepository;
